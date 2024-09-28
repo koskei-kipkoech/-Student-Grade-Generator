7f574df8792459d361561a8fefa50e1455b30fa0
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-28 12:34:54 +0300

    Updates  on index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //code for Student Grade Generator
+//Function to calculate and return grades based on input  
 function calculateGrade(){
-    let marks = prompt("input marks ")
-    marks = parseInt(marks);
-    if (isNaN (marks)|| marks < 0 || marks > 100){
+    let marks = prompt("input marks ")//prompt user for input 
+    marks = parseInt(marks);// parse input to interger
+    if (isNaN (marks)|| marks < 0 || marks > 100){//check if marks is not a number or outside valid range
         return "Invalid Input! Enter a number between between 0 to 100"
     }
-    
+    //determine and return grade based on marks range
     if (marks > 79 && marks <= 100){
         return "You have an A!"
     }
@@ -21,7 +22,7 @@ function calculateGrade(){
     else if(marks >=0 && marks <=39) {
         return "You have an E"
     }
-    
+    //optional 
     // else{
     //     return " Invalid Input! Enter a number between between 0 to 100  "
     // }
